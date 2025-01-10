f7c2f13057e96d044b85698d44133ec92dbe392b
Robert Calef <rcalef@mit.edu>
2025-01-10 17:09:03 -0500

    Update setup to use uv, add some secondary structure explorations

---
+++ README.md
@@ -0,0 +1,12 @@
+# Magneton
+
+Repo for substructure-aware representation learning and benchmarking project.
+
+## Setup
+
+Install using [`uv`](https://docs.astral.sh/uv/). Given `uv` installed using instructions from their website, install this package as:
+```
+uv sync
+uv pip install -e .
+source .venv/bin/activate
+```
--- magneton/types.py
+++ magneton/types.py
@@ -1,7 +1,60 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from enum import Enum, auto, unique
 from pprint import pprint
 from typing import List, Tuple
 
+
+
+@unique
+class DsspType(Enum):
+    H = auto()
+    E = auto()
+    G = auto()
+    I = auto()
+    P = auto()
+    T = auto()
+    S = auto()
+    # In place of ' ' (space) for OTHER
+    X = auto()
+
+DSSP_TO_NAME = [
+    "Alphahelix",
+    "Betabridge",
+    "Strand",
+    "Helix_3",
+    "Helix_5",
+    "Helix_PPII",
+    "Turn",
+    "Bend",
+    "Loop",
+]
+NAME_TO_DSSP = {
+    name: i+1 for i, name in enumerate(DSSP_TO_NAME)
+}
+
+DSSP_TO_MMCIF = [
+    "HELX_RH_AL_P",
+    "STRN",
+    "STRN",
+    "HELX_RH_3T_P",
+    "HELX_RH_PI_P",
+    "HELX_LH_PP_P",
+    "TURN_TY1_P",
+    "BEND",
+    "OTHER",
+]
+
+MMCIF_TO_DSSP = {
+    mmcif: i+1 for i, mmcif in enumerate(DSSP_TO_MMCIF)
+}
+
+@dataclass
+class SecondaryStructure:
+    dssp_type: DsspType
+    # Note that positions are 1-indexed, as output by dssp.
+    # Coordinates are half-open, i.e. [start, end)
+    start: int
+    end: int
 
 @dataclass
 class InterproEntry:
@@ -10,6 +63,7 @@ class InterproEntry:
     match_id: str
     element_name: str
     representative: bool
+    # Note that positions are 1-indexed, i.e. exactly as given in InterPro.
     positions: List[Tuple[int]]
 
 @dataclass
@@ -21,6 +75,11 @@ class Protein:
     parsed_entries: int
     total_entries: int
     entries: List[InterproEntry]
+    secondary_structs: List[SecondaryStructure] = field(default_factory=list)
+#    secondary_structs: List[SecondaryStructure]
 
     def print(self):
         pprint(self)
+
+    def __setstate__(self, state):
+        return self.__init__(**state)
